a0dff0ca289df3089477b029d46062f445da2bdc
Connor <34824423+EasterLee@users.noreply.github.com>
2026-05-13 21:44:41 -0400

    working gol

--- gol/gol.ts
+++ gol/gol.ts
@@ -9,6 +9,9 @@ async function main() {
 	const adapter = await navigator.gpu.requestAdapter();
 	const device = (await adapter?.requestDevice({
 		requiredFeatures: ["timestamp-query"],
+		requiredLimits: {
+			maxTextureDimension2D: 13950,
+		},
 	})) as GPUDevice;
 	if (!device) {
 		throw new Error("Need a browser that support WebGPU");
@@ -17,22 +20,11 @@ async function main() {
 	const presentationFormat = navigator.gpu.getPreferredCanvasFormat();
 	const canvas = document.querySelector("canvas") as HTMLCanvasElement;
 	const context = canvas.getContext("webgpu") as GPUCanvasContext;
+	canvas.style.imageRendering = "pixelated";
 
 	const shaderModule = device.createShaderModule({
 		label: "Render Shader",
 		code: shaderSrc,
-	});
-
-	const pipeLine = device.createRenderPipeline({
-		label: "Render Pipeline",
-		layout: "auto",
-		vertex: {
-			module: shaderModule,
-		},
-		fragment: {
-			module: shaderModule,
-			targets: [{ format: presentationFormat }],
-		},
 	});
 
 	const renderPassDescriptor: GPURenderPassDescriptor = {
@@ -40,7 +32,7 @@ async function main() {
 		colorAttachments: [
 			{
 				view: undefined as unknown as GPUTextureView,
-				clearValue: [0.1, 0.1, 0.1, 1.0],
+				clearValue: [0.0, 0.0, 0.0, 1.0],
 				loadOp: "clear",
 				storeOp: "store",
 			},
@@ -55,12 +47,14 @@ async function main() {
 		usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
 	});
 
+	const ratio = 0.25;
+
 	{
 		function resizeCanvas() {
 			canvas.style.width = "100vw";
 			canvas.style.height = "100vh";
-			canvas.width = canvas.clientWidth * devicePixelRatio; 
-			canvas.height = canvas.clientHeight * devicePixelRatio;
+			canvas.width = canvas.clientWidth * ratio;
+			canvas.height = canvas.clientHeight * ratio;
 			context.configure({
 				device: device,
 				format: presentationFormat,
@@ -74,7 +68,7 @@ async function main() {
 		function mouseMoveCanvas(e: MouseEvent) {
 			const x = e.x;
 			const y = e.y;
-			uiBufferValue.set([x* devicePixelRatio, y* devicePixelRatio], 2);
+			uiBufferValue.set([x * ratio, y * ratio], 2);
 			device.queue.writeBuffer(uiBuffer, SIZE.vec2, uiBufferValue, 2, 2);
 			console.log("Mouse move canvas at: ", x, y);
 		}
@@ -99,30 +93,120 @@ async function main() {
 		window.addEventListener("mousedown", mouseOn);
 		window.addEventListener("mouseup", mouseOff);
 	}
+	let textureRead = device.createTexture({
+		size: [canvas.width, canvas.height],
+		format: presentationFormat,
+		usage: GPUTextureUsage.RENDER_ATTACHMENT | GPUTextureUsage.TEXTURE_BINDING,
+	});
+
+	let textureWrite = device.createTexture({
+		size: [canvas.width, canvas.height],
+		format: presentationFormat,
+		usage: GPUTextureUsage.RENDER_ATTACHMENT | GPUTextureUsage.TEXTURE_BINDING,
+	});
+
+	const renderPipeline = device.createRenderPipeline({
+		label: "Render Pipeline",
+		layout: "auto",
+		vertex: {
+			module: shaderModule,
+		},
+		fragment: {
+			module: shaderModule,
+			targets: [{ format: presentationFormat }],
+		},
+	});
 
 	const bindGroup = device.createBindGroup({
 		label: "bind group for objects",
-		layout: pipeLine.getBindGroupLayout(0),
+		layout: renderPipeline.getBindGroupLayout(0),
 		entries: [{ binding: 0, resource: uiBuffer }],
+	});
+	let texBindGroupA = device.createBindGroup({
+		label: "bind group for objects",
+		layout: renderPipeline.getBindGroupLayout(1),
+		entries: [
+			{
+				binding: 0,
+				resource: device.createSampler({
+					magFilter: "nearest",
+					minFilter: "nearest",
+				}),
+			},
+			{ binding: 1, resource: textureRead.createView() }, // your texture as input
+		],
+	});
+	let texBindGroupB = device.createBindGroup({
+		label: "bind group for objects",
+		layout: renderPipeline.getBindGroupLayout(1),
+		entries: [
+			{
+				binding: 0,
+				resource: device.createSampler({
+					magFilter: "nearest",
+					minFilter: "nearest",
+				}),
+			},
+			{ binding: 1, resource: textureWrite.createView() }, // your texture as input
+		],
 	});
 
 	function render() {
 		renderPassDescriptor.colorAttachments[0]!.view = context
 			.getCurrentTexture()
 			.createView();
+		const encoder = device.createCommandEncoder({ label: "Render Encoder" });
 
-		const encoder = device.createCommandEncoder({ label: "our encoder" });
-
-		const pass = encoder.beginRenderPass(renderPassDescriptor);
-
-		pass.setPipeline(pipeLine);
-		pass.setBindGroup(0, bindGroup);
-		pass.draw(6);
-		pass.end();
+		const renderPass = encoder.beginRenderPass({
+			colorAttachments: [
+				{
+					view: context.getCurrentTexture().createView(), // canvas as output
+					loadOp: "clear",
+					storeOp: "store",
+					clearValue: { r: 0, g: 0, b: 0, a: 1 },
+				},
+			],
+		});
+		renderPass.setPipeline(renderPipeline);
+		renderPass.setBindGroup(0, bindGroup);
+		renderPass.setBindGroup(1, texBindGroupA);
+		renderPass.draw(6);
+		renderPass.end();
+		const writeTexturePassDescriptor: GPURenderPassDescriptor = {
+			label: "Render Texture Pass Descriptor",
+			colorAttachments: [
+				{
+					view: textureWrite.createView(),
+					clearValue: [0.0, 0.0, 0.0, 1.0],
+					loadOp: "clear",
+					storeOp: "store",
+				},
+			],
+		};
+		const writePass = encoder.beginRenderPass(writeTexturePassDescriptor);
+		writePass.setPipeline(renderPipeline);
+		writePass.setBindGroup(0, bindGroup);
+		writePass.setBindGroup(1, texBindGroupA);
+		writePass.draw(6);
+		writePass.end();
 
 		device.queue.submit([encoder.finish()]);
-		requestAnimationFrame(render);
+		// Swap the textures for the next frame
+		[textureRead, textureWrite] = [textureWrite, textureRead];
+		[texBindGroupA, texBindGroupB] = [texBindGroupB, texBindGroupA];
 	}
-	requestAnimationFrame(render);
+	let lastFrame = 0;
+	const fps = 60;
+	const frameInterval = 1000 / fps;
+	function animate(timestamp: number) {
+		requestAnimationFrame(animate);
+
+		const delta = timestamp - lastFrame;
+		if (delta > frameInterval) {
+			render();
+			lastFrame = timestamp - (delta % frameInterval);
+		}
+	}
+	requestAnimationFrame(animate);
 }
 await main();
--- gol/gol.wgsl
+++ gol/gol.wgsl
@@ -12,6 +12,8 @@ struct VSOutput {
 
 
 @group(0) @binding(0) var<uniform> ui: UI;
+@group(1) @binding(0) var mySampler: sampler;
+@group(1) @binding(1) var myTexture: texture_2d<f32>;
 
 @vertex fn vs(
     @builtin(vertex_index) vertexIndex: u32,
@@ -39,16 +41,54 @@ struct VSOutput {
 
 
     var vsOut: VSOutput;
-    vsOut.position = vec4f(flip, 0.0, 1.0);
-    // vsOut.position = vec4f(verts[vertexIndex], 0.0, 1.0);
+    //vsOut.position = vec4f(flip, 0.0, 1.0);
+    vsOut.position = vec4f(verts[vertexIndex], 0.0, 1.0);
     vsOut.color = vec4f(1.0, 1.0, 1.0, ui.mouseOver);
     vsOut.uv = verts[vertexIndex];
     return vsOut;
 }
 
+fn toClip(vert: vec2f) -> vec4f {
+    let offset = vert/ui.resolution; // 0 <-> 1
+    let two = offset * 2; // 0 <-> 1 to 0 <-> 2 space
+    let clipSpace = two - 1; // 0 <-> 2 to -1 <-> 1
+    let flip = clipSpace * vec2f(1.0, -1.0); // flip y
+    return vec4f(flip, 0.0, 1.0);
+}
+
+fn toAbsolute(vert: vec2f) -> vec2f {
+    let abs = ((vert * vec2f(1.0, -1.0)) + 1.0) / 2.0 * ui.resolution;
+    return abs;
+}
+
 @fragment fn fs(vsOut: VSOutput) -> @location(0) vec4f {
-    if (vsOut.color.a > 0.0) {
-        return vsOut.color;
+    let newUV = vsOut.uv * vec2f(0.5, -0.5) + 0.5;
+    var color = textureSample(myTexture, mySampler, newUV);
+    let unit = vec2f(1.0, 1.0)/ui.resolution;
+    let tl = textureSample(myTexture, mySampler, newUV + vec2f(-1.0, 1.0)*unit);
+    let tm = textureSample(myTexture, mySampler, newUV + vec2f(0.0, 1.0)*unit);
+    let tr = textureSample(myTexture, mySampler, newUV + vec2f(1.0, 1.0)*unit);
+    let ml = textureSample(myTexture, mySampler, newUV + vec2f(-1.0, 0.0)*unit);
+    let mm = textureSample(myTexture, mySampler, newUV + vec2f(0.0, 0.0)*unit);
+    let mr = textureSample(myTexture, mySampler, newUV + vec2f(1.0, 0.0)*unit);
+    let bl = textureSample(myTexture, mySampler, newUV + vec2f(-1.0, -1.0)*unit);
+    let bm = textureSample(myTexture, mySampler, newUV + vec2f(0.0, -1.0)*unit);
+    let br = textureSample(myTexture, mySampler, newUV + vec2f(1.0, -1.0)*unit);
+    let alive = mm.r > 0.5;
+    let aliveNear = tl.r + tm.r + tr.r + ml.r + mr.r + bl.r + bm.r + br.r;
+    if (alive){
+        if (aliveNear < 2.0 || aliveNear > 3.0) {
+            color = vec4f(0.0, 0.0, 0.0, 1.0);
+        }
+    } else {
+        if (aliveNear == 3.0) {
+            color = vec4f(1.0, 0.0, 0.0, 1.0);
+        }
     }
-    return vec4f(0.0, 0.0, 0.0, 1.0);
+
+    let offset = toAbsolute(vsOut.uv) - ui.mouse;
+    if (dot(offset, offset) < 50.0 && ui.mouseOver > 0.0) {
+        return vec4f(1.0, 0.0, 0.0, 1.0);
+    }
+    return color;
 }
