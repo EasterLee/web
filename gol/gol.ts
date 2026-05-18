0d79851946c41fff28d120e70056638eebc45df3
Connor <34824423+EasterLee@users.noreply.github.com>
2026-05-18 06:53:37 -0400

    separate painting layer and simulation layer

--- gol/gol.ts
+++ gol/gol.ts
@@ -32,15 +32,91 @@ async function main() {
 		colorAttachments: [
 			{
 				view: undefined as unknown as GPUTextureView,
-				clearValue: [0.0, 0.0, 0.0, 1.0],
+				clearValue: [0, 0, 0, 1],
 				loadOp: "clear",
 				storeOp: "store",
 			},
 		],
 	};
+	const paintPassDescriptor: GPURenderPassDescriptor = {
+		label: "Render Pass Descriptor",
+		colorAttachments: [
+			{
+				view: undefined as unknown as GPUTextureView,
+				loadOp: "load",
+				storeOp: "store",
+			},
+		],
+	};
+	const paintPipeline = device.createRenderPipeline({
+		label: "Paint Pipeline",
+		layout: "auto",
+		vertex: {
+			module: shaderModule,
+			entryPoint: "vsPaint",
+		},
+		fragment: {
+			module: shaderModule,
+			targets: [{ format: presentationFormat }],
+			entryPoint: "fsPaint",
+		},
+	});
 
-	let uiBufferValue = new Float32Array(5);
-	let uiBufferSize = SIZE.vec2 * 3; //resolution + mouse position + padding
+	const renderPipeline = device.createRenderPipeline({
+		label: "Render Pipeline",
+		layout: "auto",
+		vertex: {
+			module: shaderModule,
+			entryPoint: "vs",
+		},
+		fragment: {
+			module: shaderModule,
+			targets: [{ format: presentationFormat }],
+			entryPoint: "fs",
+		},
+	});
+
+	function createBlankTexture() {
+		return device.createTexture({
+			size: [canvas.width, canvas.height],
+			format: presentationFormat,
+			usage:
+				GPUTextureUsage.RENDER_ATTACHMENT |
+				GPUTextureUsage.TEXTURE_BINDING |
+				GPUTextureUsage.COPY_SRC,
+		});
+	}
+	function createBindGroup1(tex: GPUTexture) {
+		return device.createBindGroup({
+			label: "bind group for objects",
+			layout: renderPipeline.getBindGroupLayout(1),
+			entries: [
+				{
+					binding: 0,
+					resource: device.createSampler({
+						magFilter: "nearest",
+						minFilter: "nearest",
+					}),
+				},
+				{ binding: 1, resource: tex.createView() }, // your texture as input
+			],
+		});
+	}
+
+	let textureA: GPUTexture;
+	let textureB: GPUTexture;
+	let texBindGroupA: GPUBindGroup;
+	let texBindGroupB: GPUBindGroup;
+
+	function createTextureAndBindGroup() {
+		textureA = createBlankTexture();
+		textureB = createBlankTexture();
+		texBindGroupA = createBindGroup1(textureA);
+		texBindGroupB = createBindGroup1(textureB);
+	}
+
+	let uiBufferValue = new Float32Array(4);
+	let uiBufferSize = SIZE.vec2 * 2; //resolution + mouse position + mouse toggle + brush size
 	const uiBuffer = device.createBuffer({
 		label: `uiBuffer`,
 		size: uiBufferSize,
@@ -48,8 +124,16 @@ async function main() {
 	});
 
 	const ratio = 0.25;
-
+	const mouseBufferMaxValue = 100;
+	const mouseBufferValue = new Float32Array(mouseBufferMaxValue * 2);
+	let mouseBufferIndex = 0;
+	const mouseBuffer = device.createBuffer({
+		label: `mouseBuffer`,
+		size: mouseBufferValue.byteLength,
+		usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
+	});
 	{
+		let timerId: ReturnType<typeof setTimeout>;
 		function resizeCanvas() {
 			canvas.style.width = "100vw";
 			canvas.style.height = "100vh";
@@ -58,63 +142,68 @@ async function main() {
 			context.configure({
 				device: device,
 				format: presentationFormat,
+				usage: GPUTextureUsage.COPY_DST,
 			});
 			uiBufferValue.set([canvas.width, canvas.height]);
-			device.queue.writeBuffer(uiBuffer, 0, uiBufferValue);
+			device.queue.writeBuffer(uiBuffer, 0, uiBufferValue, 0, 2);
+
+			createTextureAndBindGroup();
+		}
+		function createResizeTimer() {
+			clearTimeout(timerId);
+			timerId = setTimeout(resizeCanvas, 250);
 		}
 		resizeCanvas();
-		window.addEventListener("resize", resizeCanvas);
+		canvas.addEventListener("resize", createResizeTimer);
 
+		function enqueueMouse(x: number, y: number) {
+			if (mouseBufferIndex >= mouseBufferMaxValue) return;
+			mouseBufferValue.set([x * ratio, y * ratio], mouseBufferIndex * 2);
+			mouseBufferIndex++;
+			console.log(mouseBufferIndex);
+		}
 		function mouseMoveCanvas(e: MouseEvent) {
-			const x = e.x;
-			const y = e.y;
-			uiBufferValue.set([x * ratio, y * ratio], 2);
-			device.queue.writeBuffer(uiBuffer, SIZE.vec2, uiBufferValue, 2, 2);
-			console.log("Mouse move canvas at: ", x, y);
+			if (!mouseOver) return;
+			const [x, y] = [e.x, e.y];
+			enqueueMouse(x, y);
+			//console.log("Mouse move canvas at: ", x, y);
 		}
 
+		let mouseOver = false;
 		function toggleMouseOverCanvas(value: boolean) {
-			uiBufferValue.set([value ? 1 : 0], 4);
-			console.log(uiBufferValue);
-			device.queue.writeBuffer(uiBuffer, SIZE.vec2 * 2, uiBufferValue, 4, 1);
+			mouseOver = value;
+			//console.log(uiBufferValue);
 		}
 
-		function mouseOn() {
+		function mouseOn(e: MouseEvent) {
+			const [x, y] = [e.x, e.y];
 			toggleMouseOverCanvas(true);
-			console.log("Mouse over canvas");
+			enqueueMouse(x, y);
+			//console.log("Mouse over canvas");
 		}
 
 		function mouseOff() {
 			toggleMouseOverCanvas(false);
-			console.log("Mouse out of canvas");
+			//console.log("Mouse out of canvas");
 		}
 
-		window.addEventListener("mousemove", mouseMoveCanvas);
-		window.addEventListener("mousedown", mouseOn);
-		window.addEventListener("mouseup", mouseOff);
+		canvas.addEventListener("mousemove", mouseMoveCanvas, {
+			passive: true,
+			capture: true,
+		});
+		canvas.addEventListener("mousedown", mouseOn);
+		canvas.addEventListener("mouseup", mouseOff);
+		canvas.addEventListener("mouseleave", mouseOff);
 	}
-	let textureRead = device.createTexture({
+
+	let texturePaint: GPUTexture = device.createTexture({
+		label: "texture Paint",
 		size: [canvas.width, canvas.height],
 		format: presentationFormat,
-		usage: GPUTextureUsage.RENDER_ATTACHMENT | GPUTextureUsage.TEXTURE_BINDING,
-	});
-
-	let textureWrite = device.createTexture({
-		size: [canvas.width, canvas.height],
-		format: presentationFormat,
-		usage: GPUTextureUsage.RENDER_ATTACHMENT | GPUTextureUsage.TEXTURE_BINDING,
-	});
-
-	const renderPipeline = device.createRenderPipeline({
-		label: "Render Pipeline",
-		layout: "auto",
-		vertex: {
-			module: shaderModule,
-		},
-		fragment: {
-			module: shaderModule,
-			targets: [{ format: presentationFormat }],
-		},
+		usage:
+			GPUTextureUsage.RENDER_ATTACHMENT |
+			GPUTextureUsage.TEXTURE_BINDING |
+			GPUTextureUsage.COPY_SRC,
 	});
 
 	const bindGroup = device.createBindGroup({
@@ -122,61 +211,48 @@ async function main() {
 		layout: renderPipeline.getBindGroupLayout(0),
 		entries: [{ binding: 0, resource: uiBuffer }],
 	});
-	let texBindGroupA = device.createBindGroup({
-		label: "bind group for objects",
-		layout: renderPipeline.getBindGroupLayout(1),
+	const paintBindGroup = device.createBindGroup({
+		label: "Paint BindGroup",
+		layout: paintPipeline.getBindGroupLayout(0),
 		entries: [
-			{
-				binding: 0,
-				resource: device.createSampler({
-					magFilter: "nearest",
-					minFilter: "nearest",
-				}),
-			},
-			{ binding: 1, resource: textureRead.createView() }, // your texture as input
+			{ binding: 0, resource: uiBuffer },
+			{ binding: 1, resource: mouseBuffer },
 		],
 	});
-	let texBindGroupB = device.createBindGroup({
-		label: "bind group for objects",
-		layout: renderPipeline.getBindGroupLayout(1),
-		entries: [
-			{
-				binding: 0,
-				resource: device.createSampler({
-					magFilter: "nearest",
-					minFilter: "nearest",
-				}),
-			},
-			{ binding: 1, resource: textureWrite.createView() }, // your texture as input
-		],
-	});
-
 	function render() {
-		renderPassDescriptor.colorAttachments[0]!.view = context
-			.getCurrentTexture()
-			.createView();
-		const encoder = device.createCommandEncoder({ label: "Render Encoder" });
+		device.queue.writeBuffer(
+			mouseBuffer,
+			0,
+			mouseBufferValue,
+			0,
+			mouseBufferIndex * 2,
+		);
+		paintPassDescriptor.colorAttachments[0]!.view = textureA.createView();
+		const encoder = device.createCommandEncoder({ label: "Paint Encoder" });
 
-		const renderPass = encoder.beginRenderPass({
-			colorAttachments: [
-				{
-					view: context.getCurrentTexture().createView(), // canvas as output
-					loadOp: "clear",
-					storeOp: "store",
-					clearValue: { r: 0, g: 0, b: 0, a: 1 },
-				},
-			],
-		});
-		renderPass.setPipeline(renderPipeline);
-		renderPass.setBindGroup(0, bindGroup);
-		renderPass.setBindGroup(1, texBindGroupA);
-		renderPass.draw(6);
+		const renderPass = encoder.beginRenderPass(paintPassDescriptor);
+		renderPass.setPipeline(paintPipeline);
+		renderPass.setBindGroup(0, paintBindGroup);
+		//renderPass.setBindGroup(1, texBindGroupA);
+		renderPass.draw(6, mouseBufferIndex);
 		renderPass.end();
+
+		encoder.copyTextureToTexture(
+			{ texture: textureA },
+			{ texture: context.getCurrentTexture() },
+			[canvas.width, canvas.height],
+		);
+
+		device.queue.submit([encoder.finish()]);
+		mouseBufferIndex = 0;
+	}
+	function simulate() {
+		const encoder = device.createCommandEncoder({ label: "Paint Encoder" });
 		const writeTexturePassDescriptor: GPURenderPassDescriptor = {
 			label: "Render Texture Pass Descriptor",
 			colorAttachments: [
 				{
-					view: textureWrite.createView(),
+					view: textureB.createView(),
 					clearValue: [0.0, 0.0, 0.0, 1.0],
 					loadOp: "clear",
 					storeOp: "store",
@@ -190,23 +266,80 @@ async function main() {
 		writePass.draw(6);
 		writePass.end();
 
+		encoder.copyTextureToTexture(
+			{ texture: textureA },
+			{ texture: context.getCurrentTexture() },
+			[canvas.width, canvas.height],
+		);
+
 		device.queue.submit([encoder.finish()]);
+
 		// Swap the textures for the next frame
-		[textureRead, textureWrite] = [textureWrite, textureRead];
+		[textureA, textureB] = [textureB, textureA];
 		[texBindGroupA, texBindGroupB] = [texBindGroupB, texBindGroupA];
 	}
 	let lastFrame = 0;
-	const fps = 60;
-	const frameInterval = 1000 / fps;
+	let fps = 60;
+	let frameInterval = 1000 / fps;
+	function changeFPS(value: number) {
+		frameInterval = 1000 / value;
+	}
 	function animate(timestamp: number) {
 		requestAnimationFrame(animate);
-
+		render();
 		const delta = timestamp - lastFrame;
 		if (delta > frameInterval) {
-			render();
+			simulate();
 			lastFrame = timestamp - (delta % frameInterval);
 		}
 	}
 	requestAnimationFrame(animate);
+
+	/**
+	 *
+	 * 	Set Configs
+	 *
+	 */
+	// Brush Size
+	const brushSliderInput = document.getElementById(
+		"brush-size-slider",
+	) as HTMLInputElement;
+	const brushNumberInput = document.getElementById(
+		"brush-size-number",
+	) as HTMLInputElement;
+
+	function setBrushSize(event: InputEvent) {
+		let value = (event.target as HTMLInputElement).value;
+		brushSliderInput.value = value;
+		brushNumberInput.value = value;
+
+		uiBufferValue.set([parseInt(value)], 2);
+		device.queue.writeBuffer(uiBuffer, SIZE.vec2, uiBufferValue, 2);
+	}
+
+	brushSliderInput.addEventListener("input", setBrushSize);
+	brushNumberInput.addEventListener("input", setBrushSize);
+
+	uiBufferValue.set([5], 2);
+	device.queue.writeBuffer(uiBuffer, SIZE.vec2, uiBufferValue, 2);
+	// FPS
+	const fpsSliderInput = document.getElementById(
+		"fps-slider",
+	) as HTMLInputElement;
+	const fpsNumberInput = document.getElementById(
+		"fps-number",
+	) as HTMLInputElement;
+
+	function onFPSEvent(event: InputEvent) {
+		let value = (event.target as HTMLInputElement).value;
+		let clamped = Math.max(0, Math.min(parseInt(value ? value : "0"), 60));
+		fpsSliderInput.value = clamped.toString();
+		fpsNumberInput.value = clamped.toString();
+
+		changeFPS(clamped);
+	}
+	fpsSliderInput.addEventListener("input", onFPSEvent);
+	fpsNumberInput.addEventListener("input", onFPSEvent);
 }
+
 await main();
--- gol/gol.wgsl
+++ gol/gol.wgsl
@@ -1,27 +1,21 @@
 struct UI {
     resolution: vec2f,
-    mouse: vec2f,
-    mouseOver: f32,
+    brushSize: f32,
 };
 struct VSOutput {
     @builtin(position) position: vec4f,
-    @location(0) color: vec4f,
-    @location(1) radius: f32,
-    @location(2) uv: vec2f,
+    @location(0) uv: vec2f,
 };
 
 
 @group(0) @binding(0) var<uniform> ui: UI;
+@group(0) @binding(1) var<uniform> mousePos: array<vec2f, 100>;
 @group(1) @binding(0) var mySampler: sampler;
 @group(1) @binding(1) var myTexture: texture_2d<f32>;
 
 @vertex fn vs(
     @builtin(vertex_index) vertexIndex: u32,
 ) -> VSOutput {
-
-	let scale = 500.0;
-	let pos = ui.mouse;
-
 
     let verts = array(
         vec2f(-1, 1), //<- top
@@ -33,17 +27,8 @@ struct VSOutput {
         vec2f(-1, 1), //left top
     );
 
-    let scaled = verts[vertexIndex] * 25.0;
-    let offset = (scaled + pos)/ui.resolution; // 0 <-> 1
-    let two = offset * 2; // 0 <-> 1 to 0 <-> 2 space
-    let clipSpace = two - 1; // 0 <-> 2 to -1 <-> 1
-    let flip = clipSpace * vec2f(1.0, -1.0); // flip y
-
-
     var vsOut: VSOutput;
-    //vsOut.position = vec4f(flip, 0.0, 1.0);
     vsOut.position = vec4f(verts[vertexIndex], 0.0, 1.0);
-    vsOut.color = vec4f(1.0, 1.0, 1.0, ui.mouseOver);
     vsOut.uv = verts[vertexIndex];
     return vsOut;
 }
@@ -62,7 +47,7 @@ fn toAbsolute(vert: vec2f) -> vec2f {
 }
 
 @fragment fn fs(vsOut: VSOutput) -> @location(0) vec4f {
-    let newUV = vsOut.uv * vec2f(0.5, -0.5) + 0.5;
+    let newUV = vsOut.uv * vec2f(0.5, -0.5) + 0.5; // texture samples uses 0 <-> 1 uv
     var color = textureSample(myTexture, mySampler, newUV);
     let unit = vec2f(1.0, 1.0)/ui.resolution;
     let tl = textureSample(myTexture, mySampler, newUV + vec2f(-1.0, 1.0)*unit);
@@ -75,20 +60,56 @@ fn toAbsolute(vert: vec2f) -> vec2f {
     let bm = textureSample(myTexture, mySampler, newUV + vec2f(0.0, -1.0)*unit);
     let br = textureSample(myTexture, mySampler, newUV + vec2f(1.0, -1.0)*unit);
     let alive = mm.r > 0.5;
-    let aliveNear = tl.r + tm.r + tr.r + ml.r + mr.r + bl.r + bm.r + br.r;
+    let aliveNear = tl.a + tm.a + tr.a + ml.a + mr.a + bl.a + bm.a + br.a;
     if (alive){
+        // make dead
         if (aliveNear < 2.0 || aliveNear > 3.0) {
-            color = vec4f(0.0, 0.0, 0.0, 1.0);
+            color = vec4f(0.0, 0.0, 0.0, 0.0);
         }
     } else {
+        // make alive
         if (aliveNear == 3.0) {
             color = vec4f(1.0, 0.0, 0.0, 1.0);
         }
     }
-
-    let offset = toAbsolute(vsOut.uv) - ui.mouse;
-    if (dot(offset, offset) < 50.0 && ui.mouseOver > 0.0) {
-        return vec4f(1.0, 0.0, 0.0, 1.0);
-    }
     return color;
+}
+
+@vertex fn vsPaint(
+    @builtin(vertex_index) vertexIndex: u32,
+    @builtin(instance_index) instanceIndex: u32,
+) -> VSOutput {
+    
+	let scale = ui.brushSize;
+	let pos =  mousePos[instanceIndex];
+    let res = ui.resolution;
+
+
+    let verts = array(
+        vec2f(-1, 1), //<- top
+        vec2f(-1, -1), //left bottom
+        vec2f(1, -1), //right bottom
+        
+        vec2f(1, -1), //right bottom
+        vec2f(1, 1), //right top
+        vec2f(-1, 1), //left top
+    );
+
+    let scaled = verts[vertexIndex] * scale;
+    let offset = (scaled + pos)/res; // 0 <-> 1
+    let two = offset * 2; // 0 <-> 1 to 0 <-> 2 space
+    let clipSpace = two - 1; // 0 <-> 2 to -1 <-> 1
+    let flip = clipSpace * vec2f(1.0, -1.0); // flip y
+
+    var vsOut: VSOutput;
+    vsOut.position = vec4f(flip, 0.0, 1.0);
+    vsOut.uv = verts[vertexIndex];
+    return vsOut;
+}
+
+@fragment fn fsPaint(vsOut: VSOutput) -> @location(0) vec4f {
+    if (dot(vsOut.uv , vsOut.uv) > 1) {
+        discard;
+    }
+    return vec4f(1.0, 0.0, 0.0, 1.0);
 }
